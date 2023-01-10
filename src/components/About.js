b80d1f9de8dd6aaf5d2d7cd1cd44168d46beaa3c
Happiness-James <gesarehjames@gmail.com>
2023-01-10 14:59:27 +0000

    assignment done

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return(
+    <div id="about">
+      <h2>About Me</h2>
+      <p>Happiness is a young self-driven woman who is a student studying software engineering at Moringa School. She is able to communicate with people at all levels and get them to visualize her intentions.  She is highly motivated and enjoys challenges within a new or existing role, and feels flexible enough to meet the needs of any project given. She can understand programming beyond the constraints of the syntax, and also comprehend the logic behind how you should implement solutions to computing problems. </p>
+      <img src={image} alt = "I made this"></img>
+    </div>
+  );
 }
 
 export default About;
